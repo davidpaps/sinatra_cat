33c7e7934f725aad6f093abebffb96e757aa377f
David Papamichael <davidpapamichael@gmail.com>
2020-01-07 13:26:57 +0000

    first commit - Sinatra returning HTML

---
+++ app.rb
@@ -0,0 +1,17 @@
+require "sinatra"
+set :session_secret, 'super secret'
+
+get '/' do
+  "Hello World"
+end
+
+
+get '/secret' do
+  "New message is new, really new"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+  <img src= 'http://bit.ly/1eze8aE' >
+  </div>"
+end
